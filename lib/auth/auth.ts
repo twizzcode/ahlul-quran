45d5500c19d642b4a529e6bc59d238e0686d7a29
twizzcode <twizzcode@gmail.com>
2026-06-08 19:16:31 +0700

    prod

--- lib/auth/auth.ts
+++ lib/auth/auth.ts
@@ -2,23 +2,30 @@ import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "@better-auth/drizzle-adapter";
 import { db } from "@/src";
 
+const isProd = process.env.NODE_ENV === "production";
+
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg",
   }),
+
   baseURL: process.env.BETTER_AUTH_URL,
-  
-  trustedOrigins: [
-    "http://lvh.me:3000",
-    "http://admin.lvh.me:3000",
-    "https://masjidsemilyartangan.com",
-    "https://admin.masjidsemilyartangan.com",
-  ],
+  secret: process.env.BETTER_AUTH_SECRET,
+
+  trustedOrigins: isProd
+    ? [
+        "https://masjidsemilyartangan.com",
+        "https://admin.masjidsemilyartangan.com",
+      ]
+    : [
+        "http://lvh.me:3000",
+        "http://admin.lvh.me:3000",
+      ],
 
   advanced: {
     crossSubDomainCookies: {
       enabled: true,
-      domain: ".lvh.me",
+      domain: isProd ? ".masjidsemilyartangan.com" : ".lvh.me",
     },
   },
 
